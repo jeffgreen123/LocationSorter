79285dfd8e6db5fd77a8b0778ab42015e2f56b45
Jeff Green <ja4green@uwaterloo.ca>
2018-03-07 09:53:22 -0500

    added , Canada to all addresses so they dont get mistaken for another country

--- addressConverter.py
+++ addressConverter.py
@@ -1,13 +1,13 @@
 from geopy.geocoders import GoogleV3
 
-fname_in = "/home/jgreen/LocationSorter/addresses.txt"
-fname_out = "/home/jgreen/LocationSorter/addressesOut.txt"
+fname_in = "addresses.txt"
+fname_out = "addressesOut.txt"
 outFile = open(fname_out,"w")
 with open(fname_in) as f:
     content = f.readlines()
 for address in content:
     geolocator =  GoogleV3(api_key = "AIzaSyBirONocHuXh5un_iDZVLfE_pg6c6vv4EQ")
-    location = geolocator.geocode(address)
+    location = geolocator.geocode(address + ", Canada")
     if location != None: 
         latitude = location.latitude
         longitude = location.longitude
